b32acd0d3ecdcb4d8a836abada78e5e958afbbe4
Steven Yang <yangchenyun@gmail.com>
2013-01-27 21:05:50 +0800

    add extra and challenging exercises set

---
+++ course-movie-rating/extra_set.sql
@@ -0,0 +1,17 @@
+-- Question 1
+-- Find the names of all reviewers who rated Gone with the Wind.
+        
+-- Question 2
+-- For any rating where the reviewer is the same as the director of the movie, return the reviewer name, movie title, and number of stars.
+        
+-- Question 3
+-- Return all reviewer names and movie names together in a single list, alphabetized.  (Sorting by the first name of the reviewer and first word in the title is fine; no need for special processing on last names or removing "The".)
+        
+-- Question 4
+-- Find the titles of all movies not reviewed by Chris Jackson.
+        
+-- Question 5
+-- For all pairs of reviewers such that both reviewers gave a rating to the same movie, return the names of both reviewers. Eliminate duplicates, don't pair reviewers with themselves, and include each pair only once. For each pair, return the names in the pair in alphabetical order.
+
+-- Question 6
+-- For each rating that is the lowest (fewest stars) currently in the database, return the reviewer name, movie title, and number of stars.
