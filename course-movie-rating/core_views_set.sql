3d2e1ca723ea1007a109ce14e1fe4bd32e8625bd
Steven Yang <yangchenyun@gmail.com>
2013-03-22 23:06:24 +0800

    added the policy for core views

--- course-movie-rating/core_views_set.sql
+++ course-movie-rating/core_views_set.sql
@@ -21,6 +21,7 @@ create view NoRating as
 
 --  Question 1
 --  Write an instead-of trigger that enables updates to the title attribute of view LateRating. 
+--  Policy: Updates to attribute title in LateRating should update Movie.title for the corresponding movie. (You may assume attribute mID is a key for table Movie.) Make sure the mID attribute of view LateRating has not also been updated -- if it has been updated, don't make any changes. Don't worry about updates to stars or ratingDate.
 
 create trigger LateRatingTitleUpdate
 instead of update of title on LateRating
@@ -33,6 +34,7 @@ end;
 
 --  Question 2
 --  Write an instead-of trigger that enables updates to the stars attribute of view LateRating. 
+--  Policy: Updates to attribute stars in LateRating should update Rating.stars for the corresponding movie rating. (You may assume attributes [mID,ratingDate] together are a key for table Rating.) Make sure the mID and ratingDate attributes of view LateRating have not also been updated -- if either one has been updated, don't make any changes. Don't worry about updates to title.
 
 create trigger LateRatingStarUpdate
 instead of update of stars on LateRating
@@ -48,6 +50,7 @@ end;
 
 --  Question 3
 --  Write an instead-of trigger that enables updates to the mID attribute of view LateRating. 
+--  Policy: Updates to attribute mID in LateRating should update Movie.mID and Rating.mID for the corresponding movie. Update all Rating tuples with the old mID, not just the ones contributing to the view. Don't worry about updates to title, stars, or ratingDate.
 create trigger LateRatingIDUpdate
 instead of update of mID on LateRating
 for each row
